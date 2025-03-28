05fb745216b71414514ec455e8aa48834962cefb
Botconex-ai <agenciabotconex@gmail.com>
2025-03-28 06:13:21 -0300

    Create index.js
    orientado pelo claude

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EnneagramTest from './EnneagramTest';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EnneagramTest />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
